284e04813628be36f30e8ef445411b6367604ca0
varunjain8 <varun.jain@osttra.com>
2024-07-04 22:04:01 +0530

    Add files via upload

---
+++ ClassTest/VarunJain/Problem3/demo/src/main/java/com/example/Item.java
@@ -0,0 +1,49 @@
+package com.example;
+
+public class Item {
+
+    private String name;
+    private Double price;
+    private String type;
+  
+    public Item() {
+    }
+  
+    public Item(String name, Double price, String type) {
+      this.name = name;
+      this.price = price;
+      this.type = type;
+    }
+  
+    public String getName() {
+      return name;
+    }
+  
+    public void setName(String name) {
+      this.name = name;
+    }
+  
+    public Double getPrice() {
+      return price;
+    }
+  
+    public void setPrice(Double price) {
+      this.price = price;
+    }
+  
+    public String getType() {
+      return type;
+    }
+  
+    public void setType(String type) {
+      this.type = type;
+    }
+
+    public String toString() {
+        return "Item [name=" + name + ", price=" + price + ", type=" + type + "]";
+    }
+
+    
+
+}
+  
