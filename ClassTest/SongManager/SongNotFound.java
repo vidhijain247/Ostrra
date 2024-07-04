4120e66208dcd25e0612de14a79422749fe8c15e
Hrithik Pandey <90620776+Heda-11@users.noreply.github.com>
2024-07-04 21:07:29 +0530

    Delete ClassTest directory

--- ClassTest/SongManager/SongNotFound.java
+++
@@ -1,7 +0,0 @@
-public class SongNotFound extends Exception {
-    @Override
-    public String getMessage() {
-        // TODO Auto-generated method stub
-        return "Song Not Found";
-    }
-}
--- ClassTest/foodagregator/src/main/java/com/foodag/Main.java
+++
@@ -1,7 +0,0 @@
-package com.foodag;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
